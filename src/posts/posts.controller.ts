a85c5131db419d3b68663aa6b39ed51fba555ebb
Guilherme <guilhermeshadow91@gmail.com>
2023-12-04 17:18:27 -0300

    feat: like and dislikes methods

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,15 +5,17 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  Req,
+  Query,
   Res,
+  Patch,
 } from '@nestjs/common';
-import { User } from '@prisma/client';
-import { Request, Response, response } from 'express';
+
+import { Response, response } from 'express';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { UserFromJwt } from 'src/auth/models/UserFromJwt';
 import { PostsDto } from './dto/posts.dto';
 import { PostsServices } from './posts.service';
+import { FiltersPostFeedbacksDto } from './dto/filtersPostsFeedback.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -37,5 +39,33 @@ export class PostsController {
     const posts = await this.postsServices.listPosts();
 
     return response.json(posts);
+  }
+
+  @Patch('/like')
+  @HttpCode(HttpStatus.OK)
+  async likedPost(
+    @Query() filters: FiltersPostFeedbacksDto,
+    @Res() response: Response,
+  ): Promise<Response<number>> {
+    const { postId, checked } = filters;
+    const likes = await this.postsServices.likedPost(postId, checked);
+
+    return response.json({
+      likes: likes,
+    });
+  }
+
+  @Patch('/dislike')
+  @HttpCode(HttpStatus.OK)
+  async dislikedPost(
+    @Query() filters: FiltersPostFeedbacksDto,
+    @Res() response: Response,
+  ): Promise<Response<number>> {
+    const { postId, checked } = filters;
+    const dislikes = await this.postsServices.dislikedPost(postId, checked);
+
+    return response.json({
+      dislikes: dislikes,
+    });
   }
 }
