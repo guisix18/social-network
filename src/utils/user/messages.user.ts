52631eee2fb0634779fb743d5e4794899c848a33
Guilherme <guilhermeshadow91@gmail.com>
2023-12-05 15:07:20 -0300

    feat: new user messages

--- src/utils/user/messages.user.ts
+++ src/utils/user/messages.user.ts
@@ -1,1 +1,3 @@
 export const CREATED_USER = 'User created successfully';
+export const USER_DEACTIVATED = 'The user has been deactivated';
+export const USER_ACTIVATED = 'The user has been activated';
