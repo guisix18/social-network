c4523ff9732cb30ca81ef8f38e1d6aa56b830475
Guilherme <guilhermeshadow91@gmail.com>
2024-01-10 17:12:03 -0300

    feat: reporter module

--- src/reporter/reporter.module.ts
+++ src/reporter/reporter.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ReporterController } from './reporter.controller';
 import { ReporterService } from './reporter.service';
+import { UserModule } from 'src/user/user.module';
+import { UserServices } from 'src/user/user.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   controllers: [ReporterController],
-  providers: [ReporterService],
+  providers: [ReporterService, UserServices],
   exports: [ReporterService],
 })
 export class ReporterModule {}
