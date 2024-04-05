3a3c9a44b1ebc3e6af2b869aa1a34a5850d148da
Guilherme <guilhermeshadow91@gmail.com>
2024-04-05 16:58:01 -0300

    feat: adding swagger documentation config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Social Project')
+    .setDescription('API for a social network')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Bearer',
+      },
+      'access_token',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3005);
 }
