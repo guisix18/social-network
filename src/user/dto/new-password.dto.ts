7d56c3df4c0ff3db84a31f611e3ad5452ad83ce5
Guilherme <guilhermeshadow91@gmail.com>
2024-04-05 16:58:46 -0300

    feat: documentation

--- src/user/dto/new-password.dto.ts
+++ src/user/dto/new-password.dto.ts
@@ -1,6 +1,12 @@
-import { IsNotEmpty, IsString, Min, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class NewPasswordDto {
+  @ApiProperty({
+    example: '12345678',
+    description:
+      'É necessário uma senha de 8 caracteres quando está no processo de alterar a senha',
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(8, {
