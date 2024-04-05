54e2f2d40399ccce5b0e8d8dfceebffe5986f117
Guilherme <guilhermeshadow91@gmail.com>
2024-04-05 16:57:25 -0300

    feat: doc with descriptions

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Comments, Post, VerifyAccount } from '@prisma/client';
 import { Transform } from 'class-transformer';
 import {
@@ -11,18 +12,34 @@ import {
 } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({
+    example: 'Famoso John Doe',
+    description:
+      'Nome do usuário, o banco atualmente aceita completo ou apenas o primeiro nome.',
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(255)
   @MinLength(1)
   name: string;
 
+  @ApiProperty({
+    example: 'johndoe@mail.com',
+    description:
+      "Email precisa ser considerado válido, formatos 'inesperados' não irá funcionar",
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: '12345678',
+    description:
+      'A senha precisa ter pelo menos 8 caracteres sendo da sua escolha o que você preferir digitar(ideal seria validação no front)',
+  })
   @IsNotEmpty()
   @IsString()
+  @MinLength(8)
   password: string;
 
   @IsOptional()
@@ -44,6 +61,11 @@ export class UserDto {
 }
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({
+    example: 'John doe',
+    description:
+      'Se caso queira atualizar o nome, deve ser fornecido para prosseguir',
+  })
   @IsOptional()
   @IsNotEmpty()
   @IsString()
@@ -51,11 +73,21 @@ export class UpdateUserDto {
   @MinLength(1)
   name: string;
 
+  @ApiPropertyOptional({
+    example: 'johndoe@mail.com',
+    description:
+      'Se caso queira atualizar o email, deve ser fornecido para prosseguir',
+  })
   @IsOptional()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiPropertyOptional({
+    example: '12345678',
+    description:
+      'Se caso queira atualizar a senha, deve ser fornecido para prosseguir',
+  })
   @IsOptional()
   @IsNotEmpty()
   @IsString()
