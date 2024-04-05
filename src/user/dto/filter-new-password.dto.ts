f7be760e97038dda6b04c62cdfa2f721f10da6e4
Guilherme <guilhermeshadow91@gmail.com>
2024-04-05 16:58:55 -0300

    feat: documentation

--- src/user/dto/filter-new-password.dto.ts
+++ src/user/dto/filter-new-password.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsJWT, IsString } from 'class-validator';
 
 export class FilterNewPasswordDto {
+  @ApiProperty({
+    example: 'ey...TOKEN',
+    description:
+      'É necessário o token enviado por email para conseguir prosseguir para criação de nova senha',
+  })
   @IsString()
   @IsJWT()
   token: string;
