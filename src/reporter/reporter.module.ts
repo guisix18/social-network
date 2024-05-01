74325e00d11b7ec533dd0be3cfc7f6849e7630e6
Guilherme <guilhermeshadow91@gmail.com>
2024-05-01 12:21:43 -0300

    feat: reporter do not need the userService

--- src/reporter/reporter.module.ts
+++ src/reporter/reporter.module.ts
@@ -3,12 +3,11 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { ReporterController } from './reporter.controller';
 import { ReporterService } from './reporter.service';
 import { UserModule } from 'src/user/user.module';
-import { UserServices } from 'src/user/user.service';
 
 @Module({
   imports: [PrismaModule, UserModule],
   controllers: [ReporterController],
-  providers: [ReporterService, UserServices],
+  providers: [ReporterService],
   exports: [ReporterService],
 })
 export class ReporterModule {}
